c5ced43b0f292edae12c2df1bfd0a848c2acdee6
peterzinho16 <peterpaul.0194@gmail.com>
2025-04-26 00:12:11 -0500

    example ssl postgres from azure connection

--- src/main/java/com/bindord/financemanagement/model/source/Parameter.java
+++ src/main/java/com/bindord/financemanagement/model/source/Parameter.java
@@ -2,8 +2,6 @@ package com.bindord.financemanagement.model.source;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
@@ -28,7 +26,6 @@ import lombok.ToString;
 public class Parameter
 {
   @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private String key;
 
   @NotBlank
