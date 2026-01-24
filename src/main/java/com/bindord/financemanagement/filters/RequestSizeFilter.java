70d99c5038b11cde5357b6f40c8f8b4095734c7f
peterzinho16 <peterpaul.0194@gmail.com>
2026-01-24 17:39:53 -0500

    minor fix in income view and modified maxRequestBodySize logic

--- src/main/java/com/bindord/financemanagement/filters/RequestSizeFilter.java
+++ src/main/java/com/bindord/financemanagement/filters/RequestSizeFilter.java
@@ -1,9 +1,13 @@
 package com.bindord.financemanagement.filters;
 
+import com.bindord.financemanagement.model.exception.ApiError;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -12,7 +16,9 @@ import java.io.IOException;
 @Component
 public class RequestSizeFilter extends OncePerRequestFilter {
 
-  private static final long MAX_REQUEST_SIZE = 5 * 1024 * 1024; // 3MB
+  @Value("${server.custom.max-content-length.in-mb}")
+  private Integer maxRequestSize;
+  private final ObjectMapper objectMapper = new ObjectMapper();
 
   @Override
   protected void doFilterInternal(HttpServletRequest request,
@@ -22,9 +28,21 @@ public class RequestSizeFilter extends OncePerRequestFilter {
 
     long size = request.getContentLengthLong();
 
-    if (size > MAX_REQUEST_SIZE) {
-      response.sendError(HttpServletResponse.SC_REQUEST_ENTITY_TOO_LARGE,
-          "Payload too large (max 5 MB)");
+    if (size > (maxRequestSize * 1024 * 1024)) {
+
+      ApiError apiError = new ApiError(
+          "Payload too large, exceeds the maximum allowed size of " + maxRequestSize + " MB",
+          "Request size exceeds the maximum allowed size of " + maxRequestSize + " MB"
+      );
+
+      response.setStatus(HttpServletResponse.SC_REQUEST_ENTITY_TOO_LARGE);
+      response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+      response.setCharacterEncoding("UTF-8");
+
+      response.getWriter().write(
+          objectMapper.writeValueAsString(apiError)
+      );
+      response.getWriter().flush();
       return;
     }
 
